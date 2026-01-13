00b3d2a71562419950c6a628afc541a8c70e19a5
qingtiann1 <93953885+qingtiann1@users.noreply.github.com>
2026-01-13 11:01:34 +0800

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -2,27 +2,55 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
 
 namespace EmbyBangumiHanimePlugin
 {
-    public class Plugin
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        public Plugin()
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, IHttpClientFactory httpClientFactory) 
+            : base(applicationPaths, xmlSerializer)
         {
+            _httpClientFactory = httpClientFactory;
+            Instance = this;
         }
 
-        public string Name => "EmbyBangumiHanimePlugin";
+        public override string Name => "EmbyBangumiHanimePlugin";
         
-        public string Description => "Anime metadata scraper for Emby";
+        public override string Description => "Anime metadata scraper for Emby";
         
-        public string Version => "1.0.0";
+        public static Plugin Instance { get; private set; }
 
-        // Placeholder methods to maintain structure
-        public Task<string> GetMetadataAsync(string query, CancellationToken cancellationToken = default)
+        public IEnumerable<PluginPageInfo> GetPages()
         {
-            return Task.FromResult($"Placeholder metadata for {query}");
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = "EmbyBangumiHanimePlugin",
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.html"
+                }
+            };
         }
+    }
+
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        public string BangumiClientId { get; set; }
+        public string BangumiClientSecret { get; set; }
+        public string BangumiRefreshToken { get; set; }
+        public long? TokenExpiryTime { get; set; }
+        public string HanimeLoginMethod { get; set; } = "credentials";
+        public string HanimeUsername { get; set; }
+        public string HanimePassword { get; set; }
+        public string HanimeCookie { get; set; }
     }
 }
